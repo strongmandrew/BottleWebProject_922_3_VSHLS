d79499b764afaf050a3f6917a3b29cf586fe35ff
Andrew <aaandrewww666@mail.ru>
2022-05-14 17:54:37 +0300

    Euler cycle new version

--- BottleWebProject_922_3_VSHLS/routes.py
+++ BottleWebProject_922_3_VSHLS/routes.py
@@ -1,13 +1,12 @@
 """
 Routes and views for the bottle application.
 """
-import bottle
+
 from bottle import route, view, template, post, request, run, HTTPResponse
 from datetime import datetime
 from pymongo import MongoClient
 from networkx import from_edgelist, is_eulerian, eulerian_circuit,circular_layout, nodes, DiGraph, draw
 from pylab import savefig, close
-
 
 @route('/')
 @route('/home')
@@ -17,7 +16,7 @@ def home():
     return dict(
         year=datetime.now().year
     )
-
+##################################################################################################
 @route('/contact')
 @view('contact')
 def contact():
@@ -27,17 +26,16 @@ def contact():
         message='Your contact page.',
         year=datetime.now().year
     )
-
+##################################################################################################
 @route('/The_Euler_cycle')
 @view('The_Euler_cycle')
 def The_Euler_cycle():
     """Renders the about page."""
     return dict(
         title='The Euler cycle',
-        message='Your application description page.',
         year=datetime.now().year
     )
-
+##################################################################################################
 @route('/Floyd')
 @view('Floyd')
 def about():
@@ -47,8 +45,7 @@ def about():
         message='Floyd method is an algorithm for finding shortest paths in a weighted graph.',
         year=datetime.now().year
     )
-
-
+##################################################################################################
 @route('/Hamilton_method')
 @view('Hamilton_method')
 def Hamilton_method(): 
@@ -57,7 +54,7 @@ def Hamilton_method():
         message='Your application description page.',
         year=datetime.now().year
     )
-
+##################################################################################################
 @route('/Dijkstras_algorithm')
 @view('Dijkstras_algorithm')
 def Dijkstras_algorithm():
@@ -67,30 +64,12 @@ def Dijkstras_algorithm():
         message='Your application description page.',
         year=datetime.now().year
     )
-
+##################################################################################################
 @post('/Euler', method='post')
 def funcEuler():
     str1= request.forms.get('Matrix_dimension').strip()
     
-    mas = str1.replace(" ", "").split(";")
-    if (mas[len(mas)-1] == ""):
-        mas.pop()
-    mas1 = []
-    for i in mas:
-        mas1.append(list(i))
-    for i in range(len(mas1)):
-        for j in range(len(mas1[i])):
-            mas1[i][j] = int(mas[i][j])
-    
-    G = DiGraph()
-    for i in range(len(mas1)):
-        G.add_node(i+1)
-
-    for i in range(len(mas1)):
-        for j in range(len(mas1[i])):
-            if(mas1[i][j] == 1):
-                G.add_edge(i+1,j+1)  
-    print(G)
+    G = function_transformation(str1)
     answer = "";
     
     if(is_eulerian(G)):
@@ -102,7 +81,30 @@ def funcEuler():
     savefig('graph.png')
     close()
     return answer
-
+##################################################################################################
+def function_transformation(str1):
+    '''функция преобразования ввода пользователя'''
+    '''возвращает объект DiGraph библиотеки networkx'''
+    str1= request.forms.get('Matrix_dimension').strip()
+    mas = str1.replace(" ", "").split(";")
+    if (mas[len(mas)-1] == ""):
+        mas.pop()
+    mas1 = []
+    for i in mas:
+        mas1.append(list(i))
+    for i in range(len(mas1)):
+        for j in range(len(mas1[i])):
+            mas1[i][j] = int(mas[i][j])
+    G = DiGraph()
+    for i in range(len(mas1)):
+        G.add_node(i+1)
+    for i in range(len(mas1)):
+        for j in range(len(mas1[i])):
+            if(mas1[i][j] == 1):
+                G.add_edge(i+1,j+1)  
+    print(G)
+    return G
+##################################################################################################
 @post('/floyd', method='post')
 def func():
     str1= request.forms.get('TEXTFEALD')
--- BottleWebProject_922_3_VSHLS/views/The_Euler_cycle.tpl
+++ BottleWebProject_922_3_VSHLS/views/The_Euler_cycle.tpl
@@ -1,25 +1,44 @@
 % rebase('layout.tpl', title='The Euler cycle', year=year)
 
-
 <head>
-    
+    <style>
+   h1 {
+    font-family: Verdana, Arial, Helvetica, sans-serif; /* Гарнитура текста */ 
+    font-size: 250%; /* Размер шрифта в процентах */ 
+   } 
+   p {
+    font-family: Verdana, Arial, Helvetica, sans-serif; 
+    font-size: 16pt; /* Размер шрифта в пунктах */ 
+   }
+   .brd {
+    border: 4px double black; /* Параметры границы */
+    background: #ffffff; /* Цвет фона */
+    padding: 10px; /* Поля вокруг текста */
+   }
+  </style>
     <link rel="stylesheet" type="text/css" href="/static/content/Stylesheet1.css" />
 </head>
-<head>
-    <body>
+<body>
     <br/>
-    <h4>Matrix dimension (whole number)</h4>
-    </body>
-    <body>
-        <form action="/Euler" method="post">
-            <p><textarea class="textboxtomatrix" type = text name="Matrix_dimension" width = "400px" height="300px" autofocus></textarea></p>
-            <p><input type="submit" value="Send"></p>
+    <h1 align="center">Euler cycle</h1>
+    <br/>
+    <p>An Euler cycle is an Euler path that is a cycle, that is, a closed path that passes through each edge of the graph exactly once. An Euler graph is a graph containing an Euler cycle.</p>
+    <form action="/Euler" method="post">
+        <p>Enter matrix: <input class="textarea_Euler" type = text name="Matrix_dimension" autofocus></textarea>
+        <input type="submit" value="Solve"></p>
 
-        </form>
-    </body>
-    <body>
-        <p><img src="../images/example1.png"  width="640" height="480" align="middle" alt="Graph example"></p>
-    </body>
-    
-</head>
-
+    </form>
+    <div class="brd" align="center">
+        <h1 align="center">Instruction how to use this method</h1>
+        <p>To find a solution, you need to write the graph matrix in a certain format.</p>
+        <p>Consider an example for a matrix.</p>
+        <p>You have a 3 x 3 matrix and you have all 1s in relationships:</p>
+        <p class="txt_algn_centr"><img src="./static/images/matrix.png" alt="Matrix example"></p>
+        <p>To enter information correctly, you need to write the graph in the correct format: </p>
+        <p>The matrix is written line by line, the separator is the character ;</p>
+        <p>For our matrix, the row will look like this: 111;111;111</p>
+        <br/>
+        <p>Graph, which the program will see:</p>
+        <p class="txt_algn_centr"><img src="./static/images/example1.png" alt="Graph example"></p>
+        </div>
+</body>
