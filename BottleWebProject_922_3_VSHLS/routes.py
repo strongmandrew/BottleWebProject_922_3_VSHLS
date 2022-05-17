bd4e40ee69c814f0778c545eeceed92bb856006d
Andrew <verigo.03@gmail.com>
2022-05-17 14:04:14 +0300

    Добавлена библиотека Js2py в env4, а также доработан метод Гамильтонова цикла

--- BottleWebProject_922_3_VSHLS/routes.py
+++ BottleWebProject_922_3_VSHLS/routes.py
@@ -153,36 +153,10 @@ def checkGraph():
     str1= request.forms.get('text')
     if (len(str1.strip()) > 0):
         if (isMatrix(str1)):
-            cnt = 0
-            for l in range(len(str1)):
-                if str1[l] == ";":
-                    cnt+=1
-            str1 = str1.replace(" ", "")
-            mas = str1.split(";")
-            mas1 = []
 
-            for i in mas:
-                mas1.append(list(i))
-            for i in range(len(mas1)):
-                for j in range(len(mas1[i])):
-                    mas1[i][j] = int(mas[i][j])
-            for k in range(cnt + 1):
-                for i in range(cnt + 1):
-                    for j in range(cnt + 1):
-                        mas1[i][j] = min(mas1[i][j], mas1[i][k] + mas1[k][j])
-
-            G = DiGraph()
-            for i in range(len(mas1)):
-                G.add_node(i+1)
-
-            for i in range(len(mas1)):
-                for j in range(len(mas1[i])):
-                    if(mas1[i][j] == 1):
-                        G.add_edge(i+1,j+1) 
+            G = function_transformation(str1.strip()) 
             
-            #graph = GraphHelper(G.edges, len(mas1[0]))
-
-            return str(G.edges)
+            return str(tournament.hamiltonian_path(G))
 
         else:
             return "Doesn't match the pattern of matrix"
