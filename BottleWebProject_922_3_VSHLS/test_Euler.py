9946bd9d2717d6804af49af6fe338a2627cdfd53
Andrew <aaandrewww666@mail.ru>
2022-05-20 18:55:30 +0300

    Тесты Euler

--- BottleWebProject_922_3_VSHLS/test_Euler.py
+++ BottleWebProject_922_3_VSHLS/test_Euler.py
@@ -8,17 +8,17 @@ class Test_test_Euler(unittest.TestCase):
                                 "11111111111111111111111111111111111111111111111111"+
                                 "11111111111111111111111111111111111111111111111111",
                                 "             1"]
-        matrix_correct_output = ["DiGraph with 3 nodes and 3 edges",
-                                 "DiGraph with 3 nodes and 3 edges",
-                                 "DiGraph with 2 nodes and 3 edges",
-                                 "DiGraph with 2 nodes and 4 edges",
-                                 "DiGraph with 3 nodes and 2 edges",
-                                 "DiGraph with 1 nodes and 0 edges",
-                                 "DiGraph with 1 nodes and 1 edges",
-                                 "DiGraph with 14 nodes and 14 edges",
-                                 "DiGraph with 28 nodes and 28 edges", 
-                                 "DiGraph with 100 nodes and 100 edges",
-                                 "DiGraph with 1 nodes and 1 edges"]
+        matrix_correct_output = ["Graph with 3 nodes and 2 edges",
+                                 "Graph with 3 nodes and 2 edges",
+                                 "Graph with 2 nodes and 1 edges",
+                                 "Graph with 2 nodes and 1 edges",
+                                 "Graph with 3 nodes and 1 edges",
+                                 "Graph with 1 nodes and 0 edges",
+                                 "Graph with 1 nodes and 0 edges",
+                                 "Graph with 14 nodes and 13 edges",
+                                 "Graph with 28 nodes and 27 edges", 
+                                 "Graph with 100 nodes and 99 edges",
+                                 "Graph with 1 nodes and 0 edges"]
         for i in range(len(matrix_correct_input)):
             self.assertEqual(str(function_transformation(matrix_correct_input[i])),matrix_correct_output[i]);
 if __name__ == '__main__':
