3d5f928643033e4b93eb97583290be14fa655fb0
Andrew <aaandrewww666@mail.ru>
2022-05-20 13:01:23 +0300

    Юнит-тесты Euler

--- BottleWebProject_922_3_VSHLS/routes.py
+++ BottleWebProject_922_3_VSHLS/routes.py
@@ -98,9 +98,22 @@ def funcEuler():
         answer+="<p class=\"txt_algn_centr\">"+result+"</p>"
         return back, answer
 ##################################################################################################
+def funcEulerCloneForUT(str1):
+    if (isMatrix(str1)):
+        G = function_transformation(str1)
+        if(is_eulerian(G)):
+            s = str(list(eulerian_circuit(G,source = 1)))
+            result = "Graph is Euler " + s[1 : -1].replace("),",") ->")
+        else:
+            result = "Graph is not Euler"
+    else:
+        result = "Doesn't match the pattern of matrix"
+    return result
+##################################################################################################
 def function_transformation(str1):
-    '''function to format user enter'''
-    '''DiGraph networkx'''
+    ###function to format user enter###
+    ###DiGraph networkx###
+    print(str1)
     mas = str1.replace(" ", "").split(";")
     if (mas[len(mas)-1] == ""):
         mas.pop()
@@ -117,6 +130,7 @@ def function_transformation(str1):
         for j in range(len(mas1[i])):
             if(mas1[i][j] == 1):
                 G.add_edge(i+1,j+1)
+    print(G)
     return G
 ##################################################################################################
 back = "<!DOCTYPE html><html><head><meta charset=\"utf-8\" /><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>SolveGraph</title><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/content/bootstrap.min.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/content/site.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"/static/content/Stylesheet1.css\" /><link rel=\"icon\" href=\"./static/images/solve_logo.PNG\" class=\"icon-stl\"/><script src=\"/static/scripts/modernizr-2.6.2.js\"></script></head><body><div class=\"navbar navbar-inverse navbar-fixed-top\"><div class=\"container\"><div class=\"navbar-header\"><button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\"><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span><span class=\"icon-bar\"></span></button><a href=\"/\"  class=\"navbar-brand\"><img class=\"logo-stl\" src=\"./static/images/solve_logo.PNG\" width=\"40\" height=\"40\"></a></div><div class=\"navbar-collapse collapse\"><ul class=\"nav navbar-nav\"><li><a href=\"/The_Euler_cycle\"><p style=\"padding-top: 10px;\">The Euler cycle</p></a></li>                    <li><a href=\"/Floyd\"><p style=\"padding-top: 10px;\">Floyd</p></a></li><li><a href=\"/Hamilton_method\"><p style=\"padding-top: 10px;\">Hamilton Method</p></a></li><li><a href=\"/Dijkstras_algorithm\"><p style=\"padding-top: 10px;\">Dijkstras algorithm</p></a></li><li><a href=\"/contact\"><p style=\"padding-top: 10px;\">Contacts</p></a></li></ul></div></div></div><div class=\"container body-content\">{{!base}}</div><script src=\"/static/scripts/jquery-1.10.2.js\"></script><script src=\"/static/scripts/bootstrap.js\"></script><script src=\"/static/scripts/respond.js\"></script></body></html>"
@@ -230,7 +244,6 @@ def checkGraph():
 def isMatrix(inputStr):
 ###function to check pattern in input string###
     matrixPattern = re.compile('^[0-1;]+$')
-    #matrixPattern = re.compile(r'^[^A-Za-z2-9/\-><?).,<>|]+$')
     if matrixPattern.match(inputStr.strip()):
         return True
     else: return False
---
+++ BottleWebProject_922_3_VSHLS/test_Euler.py
@@ -0,0 +1,16 @@
+import unittest
+from routes import function_transformation
+class Test_test_Euler(unittest.TestCase):
+    
+    def test_T(self):
+        matrix_correct_input = ["1;1;1","01;01;01","10;11","11;11","1;001","0","1","11111111111111","1111111111111111111111111111",
+                                "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"]
+        matrix_correct_output = ["DiGraph with 3 nodes and 3 edges","DiGraph with 3 nodes and 3 edges",
+                                 "DiGraph with 2 nodes and 3 edges","DiGraph with 2 nodes and 4 edges",
+                                 "DiGraph with 3 nodes and 2 edges","DiGraph with 1 nodes and 0 edges",
+                                 "DiGraph with 1 nodes and 1 edges","DiGraph with 14 nodes and 14 edges",
+                                 "DiGraph with 28 nodes and 28 edges", "DiGraph with 100 nodes and 100 edges"]
+        for i in range(len(matrix_correct_input)):
+            self.assertEqual(str(function_transformation(matrix_correct_input[i])),matrix_correct_output[i]);
+if __name__ == '__main__':
+    unittest.main()
